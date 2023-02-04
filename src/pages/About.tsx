e3edc6ec96b8be48218d5fddfffd84c12e8d6003
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2023-02-04 17:38:18 +0100

    Added first image in Services

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,11 +7,11 @@ interface AboutProps {
 
 export default function About({ language }: AboutProps) {
   const text: Record<Language, string> = {
-    'en': `The passion for cinema and its ability to convey stories of true impact at all levels, bring together two professionals in writing, film production and artistic direction on a mission to transform the vision of the world and culture into an accessible enviroment for all eyes and hearts.
+    en: `The passion for cinema and its ability to convey stories of true impact at all levels, bring together two professionals in writing, film production and artistic direction on a mission to transform the vision of the world and culture into an accessible enviroment for all eyes and hearts.
     Creativity and passion are the main aspects that define the story, values and pillars of our company. We exercise storytelling to paint the surfaces we are given, from simple to complex, from comforts to discomforts, from the land we were born to the skies we look up to.`,
-    'pt': `A paixão pelo cinema e a sua habilidade de transmitir histórias de verdadeiro impacto a todos os níveis, unem dois profissionais da escrita, audiovisual e da direção artística numa missão para transformar a visão do mundo e da cultura num contexto acessível a todos os olhos e corações.
-    Criatividade e paixão são os aspectos principais que definem a história, os valores e pilares da nossa organização. Exercitamos o storytelling para pintar as superfícies que nos são dadas, do simples ao complexo, dos confortos aos desconfortos, da terra que nascemos ao céu que olhamos.`
-  }
+    pt: `A paixão pelo cinema e a sua habilidade de transmitir histórias de verdadeiro impacto a todos os níveis, unem dois profissionais da escrita, audiovisual e da direção artística numa missão para transformar a visão do mundo e da cultura num contexto acessível a todos os olhos e corações.
+    Criatividade e paixão são os aspectos principais que definem a história, os valores e pilares da nossa organização. Exercitamos o storytelling para pintar as superfícies que nos são dadas, do simples ao complexo, dos confortos aos desconfortos, da terra que nascemos ao céu que olhamos.`,
+  };
 
   const styles: Record<string, React.CSSProperties> = {
     main: {
@@ -20,7 +20,7 @@ export default function About({ language }: AboutProps) {
       alignItems: "center",
       width: "100%",
       height: "100vh",
-     border: '3px solid orange',
+      border: "3px solid orange",
     },
   };
 
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 
+import SeloBranding from '../assets/services/selo-branding-min.png';
+
 export default function Services() {
+
+  // const GRID_MAX_WIDTH = 600;
+  // const PADDING = 25;
+  const UNIT = 100;
+  const seloBradingFactor = 3.5;
+
   const styles: Record<string, React.CSSProperties> = {
     main: {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
       width: "100%",
-      height: '100vh',
+      height: "100vh",
       border: "3px solid blue",
     },
   };
+
+
   return (
     <div id="services" style={styles.main}>
       <text>Services section</text>
+      <img src={SeloBranding} alt="" width={UNIT * seloBradingFactor} />
     </div>
   );
 }

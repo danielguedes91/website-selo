e995c9489d45b191dfe4a4b1707f5a17ee862224
Gonçalo Ferreira <goncalo.ferreira@datathings.com>
2023-04-12 20:48:13 +0200

    Sleeping page added, not implemented

---
+++ src/pages/SleepingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SleepingPage: React.FC = () => {
+     return (
+          <div style={{ position: 'absolute', width: '100vh', height: '100vh', backgroundColor: 'pink' }}>
+               {/* Selo logo */}
+          </div>
+     )
+}
+
+export default SleepingPage;
